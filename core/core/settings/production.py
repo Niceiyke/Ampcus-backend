60c0f183b68dc21ce506355969da8ac33e268c5c
Niceiyke <74918186+Niceiyke@users.noreply.github.com>
2023-12-02 15:43:57 +0100

    settings configuration (#1)
    * modularize settings
    
    * update
    
    ---------
    
    Co-authored-by: niceiyke <niceiyke04@gmail.com>

---
+++ core/core/settings/production.py
@@ -0,0 +1,25 @@
+from .base import *
+
+
+ALLOWED_HOSTS = ["*"]
+
+
+DATABASES = {
+    "default": {
+        "ENGINE": "django.db.backends.postgresql",
+        "NAME": os.environ.get("POSTGRES_DATABASE"),
+        "USER": os.environ.get("POSTGRES_USER"),
+        "PASSWORD": os.environ.get("POSTGRES_PASSWORD"),
+        "HOST": os.environ.get("POSTGRES_HOST"),
+        "PORT": os.environ.get("POSTGRES_PORT"),
+    }
+}
+
+
+EMAIL_BACKEND = "django.core.mail.backends.smtp.EmailBackend"
+EMAIL_HOST = "smtp.gmail.com"
+EMAIL_HOST_USER = "oyomworld@gmail.com"
+EMAIL_HOST_PASSWORD = os.environ.get("EMAIL_HOST_PASSWORD")
+EMAIL_PORT = 587
+EMAIL_USE_TLS = True
+EMAIL_USE_SSL = False
