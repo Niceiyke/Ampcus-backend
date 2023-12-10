6451159bce05ff52104aa0d2aa33acf9cc2b331c
niceiyke <oyomworld@gmail.com>
2023-12-10 22:29:24 +0100

    update

--- core/accounts/signals.py
+++ core/accounts/signals.py
@@ -1,4 +1,5 @@
 from django.db.models.signals import post_save
+import os
 from django.dispatch import receiver
 from members.models import Member
 from .models import CustomUser
@@ -27,20 +28,28 @@ def send_successefull_signup(sender, instance, created, **kwargs):
     if base.PRODUCTION == "True":
         s = pyshorteners.Shortener()
         short_url = s.tinyurl.short(url)
-        print("Shortened URL:", short_url)
-        print("email", settings.EMAIL_HOST_USER)
+
         message = f"Thank you for registering with Ama Cooperative Society.\nuse this link {short_url} to update your profile "
         subject = "Successfull Registration "
         message = message
-        from_email = "iyke@gmail.com"
+        from_email = "iykedave04@gmail.com"
         recipient_list = [instance.email]
         if created:
-            send_mail(subject, message, from_email, recipient_list, fail_silently=False)
+            send_mail(
+                subject=subject,
+                message=message,
+                from_email=from_email,
+                recipient_list=recipient_list,
+                fail_silently=False,
+                auth_user=settings.EMAIL_HOST_USER,
+                auth_password=settings.EMAIL_HOST_PASSWORD,
+            )
+            # send_mail(subject, message, from_email, recipient_list, fail_silently=False)
     else:
         message = f"Thank you for registering with Ama Cooperative Society.\nuse this link {url} to update your profile "
         subject = "Successfull Registration "
         message = message
-        from_email = "iyke@gmail.com"
+        from_email = "ortld@gmail.com"
         recipient_list = [instance.email]
         if created:
             send_mail(subject, message, from_email, recipient_list, fail_silently=False)
